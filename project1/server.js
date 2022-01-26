b7b3cff6d4466eee9dff4a8a95ea74b98d6ab823
WooJin Lim <faith2296@sch.ac.kr>
2022-01-27 02:58:16 +0900

    asdf

--- project1/server.js
+++ project1/server.js
@@ -3,6 +3,11 @@ const server = express()
 
 server.use(express.static('public'))
 server.use('/login', require('./routes/login'))
+server.use('/page1', require('./routes/page1'))
+server.use('/page2', require('./routes/page2'))
+server.use('/page3', require('./routes/page3'))
+server.use('/page4', require('./routes/page4'))
+server.use('/page5', require('./routes/page5'))
 server.use('/', require('./routes/main'))
 server.set('view engine', 'ejs')
 
