4fe7806e4693704970e36e4b83e6debc7f83e0a5
WooJin Lim <95209789+woojin2296@users.noreply.github.com>
2022-02-05 21:15:03 +0900

    asdf

--- project1/routes/page/page1.js
+++ project1/routes/page/page1.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get('', (req, res) => {
-    res.render(path.join(__dirname, '../../views/page'), { page: 1, name: ""})
+    res.sendFile(path.join(__dirname, '../../views/page1.html'))
 })
 
 module.exports = router
