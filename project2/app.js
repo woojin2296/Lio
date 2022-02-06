8326955bd9be1fd3011ec244b8096b318599b75f
WooJin Lim <95209789+woojin2296@users.noreply.github.com>
2022-02-06 21:45:24 +0900

    Update app.js

--- project2/app.js
+++ project2/app.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const path = require('path')
 
+const app = express()
+
+app
+  .use(express.json())
+  .use(express.urlencoded({extended: false}))
+
+app
+  .get("/", (req, res) => {
+      return res.sendFile
+  })
